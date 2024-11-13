36e70e9b91b84dcce253716433c52d7636a5b371
John Isaac Latigay <jclatigay@up.edu.ph>
2024-11-14 01:48:46 +0800

    edit password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
