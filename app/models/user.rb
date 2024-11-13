e05a0361061aa5984f15f221d62ae77b3f321ca5
John Isaac Latigay <jclatigay@up.edu.ph>
2024-11-14 01:09:37 +0800

    user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  validates :password, presence: true
+end
