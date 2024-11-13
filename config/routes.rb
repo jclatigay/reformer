289e7b8ef870111fbc8136c882042bedae26a893
John Isaac Latigay <jclatigay@up.edu.ph>
2024-11-14 01:29:19 +0800

    logout and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
